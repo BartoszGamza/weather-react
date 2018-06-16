d7bc12b3e2c6c0c8bbd96091b73fabd0ac61a723
bartoszgamza <bartosz.gamza12@gmail.com>
2018-06-16 13:19:20 +0200

    inital commit

---
+++ src/components/Tittle.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Tittle extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Simple React Weather App by Bartosz Gamza
+        </h1>
+        <p>
+          Type in city and country to display data
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Tittle;
